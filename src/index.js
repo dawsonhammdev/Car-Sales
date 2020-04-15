10b6f99f3b6878ad9e9057051fce7ee327eeb6f6
dawsonhammdev <hammsdesigns@gmail.com>
2020-04-15 17:03:50 -0500

    I have added createStore and provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+    );
